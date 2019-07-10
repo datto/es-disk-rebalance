c8a3587ec85f2a511c077c5a2e04e72c9b707db0
Alex Parrill <aparrill@datto.com>
2019-07-10 09:59:40 -0400

    Bunch of changes and tweaks
    * Keep track of non-started shards to ensure that they don't break movement
    * Don't swap shards of similar sizes
    * Log the plan better

--- es-rebalance/__main__.py
+++ es-rebalance/__main__.py
@@ -7,6 +7,7 @@ import argparse
 import json
 import logging
 import pprint
+import re
 import statistics
 
 LOG = logging.getLogger(__name__)
@@ -23,11 +24,26 @@ Shard = namedtuple("Shard", [
 	"shard",
 	"prirep",
 	"store",
+	"can_move",
 ])
 
+RELOCATING_RE = re.compile(r"^([^ ]+) -> [^ ]+ [^ ]+ ([^ ]+)$")
+
+def formate_bytes(num_bytes):
+	if num_bytes >= 1024*1024*1024*1024:
+		return "%.2fTiB" % (num_bytes / (1024*1024*1024*1024))
+	if num_bytes >= 1024*1024*1024:
+		return "%.2fGiB" % (num_bytes / (1024*1024*1024))
+	if num_bytes >= 1024*1024:
+		return "%.2fMiB" % (num_bytes / (1024*1024))
+	if num_bytes >= 1024:
+		return "%.2fKiB" % (num_bytes / (1024))
+	return "%dB" % num_bytes
+
 class Plan:
-	def __init__(self, es, box_type):
+	def __init__(self, es, box_type, size_percent_threshold):
 		self.es = es
+		self.size_fraction_threshold = 1 - size_percent_threshold / 100
 		raw_alloc_infos = es.cat.allocation(format="json", bytes="b")
 		raw_shards = es.cat.shards(format="json", bytes="b")
 		raw_nodes = dict((node["name"], node) for node in es.nodes.info(format="json")["nodes"].values())
@@ -45,22 +61,32 @@ class Plan:
 			)
 		
 		for shard in raw_shards:
-			if shard["state"] != "STARTED":
-				LOG.info("Ignoring shard %s %s on %s: state isn't started, it's %s",
-					shard["index"], shard["shard"], shard["node"], shard["state"])
-				continue
-			if shard["node"] not in nodes:
-				continue
-			nodes[shard["node"]].shards.append(Shard(
-				index=shard["index"],
-				shard=int(shard["shard"]),
-				prirep=shard["prirep"],
-				store=int(shard["store"]),
-			))
+			if shard["state"] == "RELOCATING":
+				match = RELOCATING_RE.match(shard["node"])
+				shard_nodes = (match.group(1), match.group(2))
+			else:
+				shard_nodes = (shard["node"],)
+			
+			can_move = shard["state"] == "STARTED"
+			if not can_move:
+				LOG.warn("Won't be able to move shard %s on host %s, state is %s",
+					shard, shard_nodes, shard["state"])
+			
+			for node_name in shard_nodes:
+				if node_name not in nodes:
+					continue
+				nodes[node_name].shards.append(Shard(
+					index=shard["index"],
+					shard=int(shard["shard"]),
+					prirep=shard["prirep"],
+					store=int(shard["store"]),
+					can_move=can_move,
+				))
 		
 		self.operations = []
 		self.nodes_by_size = list(nodes.values())
 		self.moved_shards = set()
+		self.shards_by_size = None
 		self._sort()
 	
 	def _sort(self):
@@ -69,12 +95,19 @@ class Plan:
 			reverse=True)
 		for node in self.nodes_by_size:
 			node.shards.sort(key=lambda shard: shard.store, reverse=True)
+		#self.shards_by_size = sorted(
+		#	((host, shard) for host in self.nodes_by_size for shard in host.shards),
+		#	key=lambda tup: tup[1].store
+		#)
 	
 	def plan_step(self):
 		current_pvariance = self.percent_used_variance()
 		
 		for big_node, big_shard in self.find_big_shards():
 			for small_node, small_shard in self.find_small_shards():
+				if small_shard.store >= big_shard.store:
+					continue
+				
 				# Can we even move this node?
 				if not self.can_exchange_shards(big_node, big_shard, small_node, small_shard):
 					continue
@@ -97,6 +130,8 @@ class Plan:
 			for shard in node.shards:
 				if (shard.index, shard.shard) in self.moved_shards:
 					continue
+				if not shard.can_move:
+					continue
 				yield node, shard
 	
 	def find_small_shards(self):
@@ -104,16 +139,32 @@ class Plan:
 			for shard in reversed(node.shards):
 				if (shard.index, shard.shard) in self.moved_shards:
 					continue
+				if not shard.can_move:
+					continue
 				yield node, shard
 	
 	def can_exchange_shards(self, node1, node1_shard, node2, node2_shard):
 		if node1 is node2:
 			return False
 		
+		# Is the swap large enough to be worth it?
+		if node1_shard.store > node2_shard.store:
+			size_fraction = node2_shard.store / node1_shard.store
+		else:
+			size_fraction = node1_shard.store / node2_shard.store
+		if size_fraction > self.size_fraction_threshold:
+			LOG.debug("Not worth swapping: shard %s/%s/%s (%s) with shard %s/%s/%s (%s)",
+				node1_shard.index, node1_shard.shard, node1_shard.prirep,
+				formate_bytes(node1_shard.store),
+				node2_shard.index, node2_shard.shard, node2_shard.prirep,
+				formate_bytes(node2_shard.store),
+			)
+			return False
+		
 		# Will the shards fit?
-		node1_used = sum((shard.store if shard is not node1_shard else 0) for shard in node1.shards) \
+		node1_used = sum(shard.store for shard in node1.shards) \
 			+ node2_shard.store
-		node2_used = sum((shard.store if shard is not node2_shard else 0) for shard in node2.shards) \
+		node2_used = sum(shard.store for shard in node2.shards) \
 			+ node1_shard.store
 		if node1_used > node1.capacity or node2_used > node2.capacity:
 			LOG.debug("Too big: %d >? {} and/or %d >? %d", node1_used, node1.capacity, node2_used, node2.capacity)
@@ -134,7 +185,12 @@ class Plan:
 			for checking_shard in node.shards:
 				if node1_shard.index == checking_shard.index and \
 					node1_shard.shard == checking_shard.shard:
-					LOG.debug("Rack conflict: shard %s would conflict with %s", node1_shard, checking_shard)
+					LOG.debug("Rack conflict: moving shard %s/%s/%s to node %s would conflict with shard %s/%s/%s on node %s",
+						node1_shard.index, node1_shard.shard, node1_shard.prirep,
+						node1.name,
+						checking_shard.index, checking_shard.shard, checking_shard.prirep,
+						node.name,
+					)
 					return False
 		# Same but for node2_shard to node1
 		for node in self.nodes_by_size:
@@ -145,12 +201,24 @@ class Plan:
 			for checking_shard in node.shards:
 				if node2_shard.index == checking_shard.index and \
 					node2_shard.shard == checking_shard.shard:
-					LOG.debug("Rack conflict: shard %s would conflict with %s", node2_shard, checking_shard)
+					LOG.debug("Rack conflict: moving shard %s/%s/%s to node %s would conflict with shard %s/%s/%s on node %s",
+						node2_shard.index, node2_shard.shard, node2_shard.prirep,
+						node2.name,
+						checking_shard.index, checking_shard.shard, checking_shard.prirep,
+						node.name,
+					)
 					return False
 		
 		return True
 	
 	def plan_exchange(self, node1, node1_shard, node2, node2_shard):
+		LOG.info("Exchanging shard %s/%s/%s (%s) on node %s (%.2f%%) with %s/%s/%s (%s) on node %s (%.2f%%)",
+			node1_shard.index, node1_shard.shard, node1_shard.prirep, formate_bytes(node1_shard.store),
+			node1.name, sum(node.store for node in node1.shards) * 100 / node1.capacity,
+			node2_shard.index, node2_shard.shard, node2_shard.prirep, formate_bytes(node2_shard.store),
+			node2.name, sum(node.store for node in node2.shards) * 100 / node2.capacity,
+		)
+		
 		self.operations.append({
 			"move": {
 				"index": node1_shard.index,
@@ -207,6 +275,8 @@ def main():
 		help="Box type of nodes to rebalance. One of 'cold' or 'hot'")
 	parser.add_argument("-i", "--iterations", type=int, default=10,
 		help="Number of shards to exchange")
+	parser.add_argument("-p", "--size-percentage", type=float, default=90,
+		help="Reject exchanges of shards whose sizes are within this percent of each other, to avoid swapping large shards around.")
 	parser.add_argument("-v", "--verbose", action="store_true",
 		help="Print debug logs")
 	parser.add_argument("--execute", action="store_true",
@@ -220,12 +290,12 @@ def main():
 	
 	es = Elasticsearch(args.url)
 	
-	plan = Plan(es, args.box_type)
+	plan = Plan(es, args.box_type, args.size_percentage)
 	for i in range(args.iterations):
 		if not plan.plan_step():
 			LOG.warn("Could not move anything, stopping early after %d iteration(s)", i+1)
 			break
-	pprint.pprint(plan.operations)
+	#pprint.pprint(plan.operations)
 	
 	plan.exec(dry_run=not args.execute)
 	if not args.execute:
